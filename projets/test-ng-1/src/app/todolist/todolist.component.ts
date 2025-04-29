d07553f64df1b64cbd8d97e57ce8f17a500ef14e
romain <romain.dugeay@gmail.com>
2025-04-29 13:27:44 +0200

    TODOLIST V1 OK

--- projets/test-ng-1/src/app/todolist/todolist.component.ts
+++ projets/test-ng-1/src/app/todolist/todolist.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todolist',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
 export class TodolistComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 
 }
